96cff742e9729368978e264cfc9b3e7f71b6e532
Sebastien <63232798+Bastien-OC20@users.noreply.github.com>
2021-04-02 22:47:14 +0200

    Add files via upload

---
+++ _nuxt/static/1617396320/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617396320",layout:"default",error:a,state:{isSoundEnabled:b},serverRendered:b,routePath:c,config:{googleAnalytics:{id:"G-LBSV9M902G"},app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
